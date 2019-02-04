0082c7b9d3ed384fbef3d9ab46b33ded750332fa
Milos Mikulasek <milos.mikulasek@gmail.com>
2019-02-04 16:29:33 +0100

    bak-mgt-ps

---
+++ move-sql-bak-files.ps1
@@ -0,0 +1,9 @@
+﻿Copy-Item -Path "Full Backups\*.bak" -Destination "\\HAT-BAC-01\NAV18Backups$\dc-ndb-01\Full Backups\" -Force
+Copy-Item -Path "Differential Backups\*.bak" -Destination "\\HAT-BAC-01\NAV18Backups$\dc-ndb-01\Differential Backups\" -Force
+Copy-Item -Path "Transaction Log Backups\*.trn" -Destination "\\HAT-BAC-01\NAV18Backups$\dc-ndb-01\Transaction Log Backups\" -Force
+
+Move-Item -Path "Full Backups\*.bak" -Destination "Full Backups\arc\" -Force
+Move-Item -Path "Differential Backups\*.bak" -Destination "Differential Backups\arc\" -Force
+Move-Item -Path "Transaction Log Backups\*.trn" -Destination "Transaction Log Backups\arc\" -Force
+
+
